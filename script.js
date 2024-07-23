5cc2819ae15c0ade094d15118273e91ad39395ad
reza salimi <168948763+Rezaslm0@users.noreply.github.com>
2024-07-23 06:42:03 +0330

    feat: upload files
    I added the html, css and js files

---
+++ script.js
@@ -0,0 +1,57 @@
+let school = {
+  classes: [
+    {
+      name: "ClassA",
+      students: [
+        {
+          name: "amir",
+          family: "beheshti",
+          phone: "09051112222",
+          address: "bushehr,bahonar",
+          grade: 20,
+        },
+        {
+            name: 'saeed',
+            family: 'jamshidi',
+            phone: '09175556666',
+            address: 'bushehr,sangi',
+            grade: 11
+        },
+        {
+            name: 'aria',
+            family: 'salehi',
+            phone: '09384441111',
+            address: 'bushehr,bahmani',
+            grade: 9
+        },
+      ],
+    },
+    {
+      name: "ClassB",
+      students: [
+        {
+            ame: 'amin',
+            family: 'taheri',
+            phone: '09370001111',
+            address: 'bushehr,imam sqare',
+            grade: 17
+        },
+        {
+            name: 'bahman',
+            family: 'eftekhar',
+            phone: '09056543211',
+            address: 'bushehr,ashoori street',
+            grade: 16.5
+        },
+        {
+            name: 'reza',
+            family: 'fazeli',
+            phone: '09203216547',
+            address: 'bushehr,niayesh',
+            grade: 19
+        },
+      ],
+    },
+  ],
+};
+console.log(school);
